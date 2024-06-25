740e218e4e4fe4deac6dea238608a0f70eb98294
Corentin Pradié <corentin.prad@gmail.com>
2024-06-25 20:23:28 +0200

    Update background image paths to use Flask static directory

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,18 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../back-end/app/static'),
-  publicPath: '/',
   devServer: {
     allowedHosts: "all",
   },
   css: {
     loaderOptions: {
       css: {
-        url: false, // Ensuring CSS background images are treated as relative to CSS file
+        url: false,
       },
     },
   },
+  outputDir: path.resolve(__dirname, '../back-end/app/static'),
   chainWebpack: config => {
     config.plugin('copy').tap(args => {
       const UNESCAPED_GLOB_SYMBOLS_RE = /(\\?)([()*?[\]{|}]|^!|[!+@](?=\())/g;
@@ -20,6 +19,6 @@ module.exports = {
       const escapePublicDir= publicDir.replace(UNESCAPED_GLOB_SYMBOLS_RE, '\\$2');
       args[0].patterns[0].globOptions.ignore = args[0].patterns[0].globOptions.ignore.map(i => i.replace(publicDir, escapePublicDir));
       return args;
-    });
+  });
   }
 };
