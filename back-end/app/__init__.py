b44d4d6c8266444d351c0532c7a58309ec7c1be0
Corentin Pradié <corentin.prad@gmail.com>
2024-06-25 20:09:21 +0200

    Add heroku-postbuild script to package.json

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,18 +1,23 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from colorama import Fore, Style
 from flask_cors import CORS
 
-
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', static_url_path='')
     CORS(app)
 
     from .routes import api as api_blueprint
     app.register_blueprint(api_blueprint)
 
+    @app.route('/')
+    def serve():
+        return send_from_directory(app.static_folder, 'index.html')
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return send_from_directory(app.static_folder, 'index.html')
+
     print(Fore.BLUE)
-    # print("Update data : http://localhost:5000/update_data")
     print("Get matches : http://localhost:5000/get_matches")
-    # print("Get Data : http://localhost:5000/get_data")
     print(Style.RESET_ALL)
     return app
